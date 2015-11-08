ef6064860cb31fc5d339081db9ddc3da67522cc8
SeanSassenrath <sean.sassenrath@gmail.com>
2015-11-07 21:18:30 -0800

    adding client side routing

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,13 +1,25 @@
+var path = require('path')
+
 module.exports = function(app, express) {
   var clientRouter = express.Router();
 
+  clientRouter.get('/', function(req, res, next) {
+    res.sendFile(path.join(__dirname+'../../../index.html'));
+  })
+
   clientRouter.use(function(req, res, next) {
     console.log("middleware test")
-    console.log(req)
+    console.log(req.headers)
+    if(req['x-access-token']) {
+      console.log("A token was sent")
+    } else {
+      console.log("No token sent, going back to home");
+      res.redirect('/')
+    }
     // var token = window.localStorage.getItem('userToken')
     // var user = window.localStorage.getItem('userProfile')
     // console.log(JSON.parse(user))
-    next();
+    // next();
   })
 
   clientRouter.get("/test", function(req, res, next) {
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8080;
+var path = require("path");
 
-var clientRoutes = require('./app/routes/routes.js')(app, express);
-app.use(clientRoutes);
+// app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/assets'))
+// app.use(express.static(__dirname + '/assets'))
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+})
+// var clientRoutes = require('./app/routes/routes.js')(app, express);
+// app.use(clientRoutes);
 
 app.listen(port);
 console.log('Blog client is spinning on port', port)
